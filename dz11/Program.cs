e0920cc0fe3ae37bb475cc92dbf5dd521a89e217
gavrilenkovi@bk.ru <gavrilenkovi@bk.ru>
2021-11-06 20:36:08 +0300

    Добавьте файлы проекта.

---
+++ dz11/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation equation = new Equation(56, 28);
+            equation.Root();          
+            Console.ReadKey();
+        }
+    }
+    public struct Equation
+    {
+        int k;
+        int B { get; set; }
+
+        public int K
+        {
+            set
+            {
+                if (value != 0)
+                {
+                    k = value;
+                }
+                else
+                {
+                    Console.WriteLine("на ноль делить нельзя");
+                }
+            }
+            get
+            {
+                return k;
+            }
+        }
+        public Equation(int k, int b)
+        {
+            K = k;
+           
+        }
+        public void Root()
+        {
+            int x = -B / k;
+            Console.WriteLine("решение уровнения x= {0}", x);
+        }
+    
+
+    }
+
+}
---
+++ dz111/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation equation = new Equation { k =34, b = 28 };
+            equation.Root();
+            Console.ReadKey();
+        }
+        struct Equation
+        {
+            public double k;
+            public double b;
+            public double K
+            {
+                set
+                {
+                    if (value != 0)
+                    {
+                        k = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("на ноль делить нельзя");
+                    }
+                }
+                get
+                {
+                    return k;
+                }
+            }
+            public void Root()
+            {
+                double x = -b / k;
+                Console.WriteLine("решение уровнения 0=kx+b с данными параметрами равно {0}",x);
+            }
+
+        }
+    }
+}
